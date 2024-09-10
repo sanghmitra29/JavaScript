9c5c6f4c0fbf0b58189dd255f4612039c269a4c6
= <sanghmitrakandpal@gmail.com>
2024-09-10 15:02:45 +0530

    datatypes added

---
+++ 1_Basics/3_Datatypes.js
@@ -0,0 +1,26 @@
+ "use strict"; //treat all js code as newer version
+
+// alert("hello") we are using nodejs, not browser
+
+// code readability should be high 
+
+let name = "sanghmitra"
+let age = 21;
+let loggedin = true
+let state;
+
+
+// PRIMITIVE DATA TYPES
+// int = 2 to power 53
+// bigint
+// string = ""
+// boolean = true / false
+// null = standalone value
+// undefined = 
+// symbol = uniqueness
+
+console.log(typeof (undefined)) // undefined
+console.log(typeof (null)) // object
+
+
+
